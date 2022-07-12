4a4134384840af524c1ea63c0a1e67604eee526c
Moontary <kostiantyntkachenko@gmail.com>
2022-07-12 15:06:19 +0300

    fix branch name

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "key_name" {
-  default = "ktkache_web_app"
+  default     = "ktkache_web_app"
   type        = string
   description = "The name for ssh key, used for aws_launch_configuration"
 }
 
 variable "cluster_name" {
-  default = "supper-pupper-app"
+  default     = "supper-pupper-app"
   type        = string
   description = "The name of AWS ECS cluster"
 }
